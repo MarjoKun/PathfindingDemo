3955ed2f7354e150512cda5326dbb6c5585210ed
Marjo-Kun <74604472+Marjo-Kun@users.noreply.github.com>
2024-05-17 21:33:48 +0200

    Created MVC base scripts

---
+++ Assets/Scripts/UI/MVC/Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    [DefaultExecutionOrder(ENSURE_EXECUTION_ORDER_FOR_INITIAL_AWAKE)]
+    public class Controller<TView, TModel> : MonoBehaviour where TView : View where TModel : Model<TView>
+	{
+		[field: SerializeField]
+		protected TView CurrentView { get; private set; }
+		[field: SerializeField]
+		protected TModel CurrentModel { get; private set; }
+
+        private const int ENSURE_EXECUTION_ORDER_FOR_INITIAL_AWAKE = -1;
+
+        private void Awake ()
+		{
+			CurrentModel.SetView(CurrentView);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/MVC/Model.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+	public class Model<TView> : MonoBehaviour where TView : View
+	{
+		protected TView CurrentView { get; private set; }
+
+		public void SetView (TView newView)
+		{
+			CurrentView = newView;
+		}
+	}
+}
